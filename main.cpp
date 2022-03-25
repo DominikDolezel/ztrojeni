a44bbb6be1df5e826d7efef019670503ee60ab5a
DominikDolezel <71893904+DominikDolezel@users.noreply.github.com>
2022-03-25 13:55:50 +0100

    všechno <3 <3

--- main.cpp
+++ main.cpp
@@ -103,17 +103,58 @@ bitset <BITU * 3> nahodnaZmena(bitset <BITU * 3> ztrojeno) {
     for (int i = 0; i < pocet_spatnych_cislic; i++) {
         cout << zmenene_cislice[i] + 1 << ". ";
     }
+    cout << endl;
     return ztrojeno;
 }
 
-void oprava(bitset <BITU * 3> zmeneno) {
-    bitset <BITU * 3>
+bitset <BITU * 3> trojitaMaska(short prvni_bit) {
+    bitset <BITU * 3> trojita_maska;
+    trojita_maska = nula;
+    for (int i = prvni_bit; i < prvni_bit + 3; i++) {
+        trojita_maska = trojita_maska | vytvorMasku(i);
+    }
+    return trojita_maska;
+}
+
+bitset <BITU * 3> oprava(bitset <BITU * 3> zmeneno) {
+    bitset <BITU * 3> maska;
+    bitset <BITU * 3> je_tu;
+    bitset <BITU * 3> opraveno;
+    opraveno = nula;
+    short pocet_jednicek;
+    for (int i = BITU - 1; i >= 0; i--) {
+        pocet_jednicek = 0;
+        //maska = trojitaMaska(i * 3);
+        for (int j = 0; j < 3; j++) {
+            maska = vytvorMasku(i * 3 + j);
+            je_tu = maska & zmeneno;
+            if (je_tu != nula) {
+                pocet_jednicek++;
+            }
+        }
+
+        if (pocet_jednicek == 3) {
+            cout << "jaja ";
+            opraveno = opraveno | trojitaMaska(i * 3);
+        } else if (pocet_jednicek == 2) {
+            cout << "falsch ";
+            opraveno = opraveno | trojitaMaska(i * 3);
+        } else if (pocet_jednicek == 1) {
+            cout << "falsch ";
+            opraveno = opraveno & ~trojitaMaska(i * 3);
+        } else {
+            cout << "jaja ";
+            opraveno = opraveno & ~trojitaMaska(i * 3);
+        }
+        cout << endl;
+    }
+    return opraveno;
 }
 
 int main()
 {
     int vstup;
-    bitset <BITU * 3> ztrojeno, zmeneno;
+    bitset <BITU * 3> ztrojeno, zmeneno, opraveno;
 
     cin >> vstup;
 
@@ -121,5 +162,7 @@ int main()
 
     zmeneno = nahodnaZmena(ztrojeno);
 
-    oprava(zmeneno);
+    opraveno = oprava(zmeneno);
+
+    cout << "opraveno: " << opraveno;
 }
